8ae2650dad08608eaaf13b43a5699dba768f76b1
arvinr11 <roeslimarvin@gmail.com>
2025-08-21 20:11:32 +0800

    feat(contact): add spotlight card to contact card

---
+++ app/components/Components/SpotlightCard/SpotlightCard.tsx
@@ -0,0 +1,94 @@
+/*
+	Installed from https://reactbits.dev/ts/tailwind/
+*/
+
+import React, { useRef, useState } from "react";
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+interface SpotlightCardProps extends React.PropsWithChildren {
+  className?: string;
+  spotlightColor?: `rgba(${number}, ${number}, ${number}, ${number})`;
+  mode?: 'single' | 'multicolor';
+}
+
+const SpotlightCard: React.FC<SpotlightCardProps> = ({
+  children,
+  className = "",
+  spotlightColor = "rgba(255, 255, 255, 0.25)",
+  mode = 'single'
+}) => {
+  const divRef = useRef<HTMLDivElement>(null);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+  const [opacity, setOpacity] = useState<number>(0);
+
+  const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    if (!divRef.current || isFocused) return;
+
+    const rect = divRef.current.getBoundingClientRect();
+    setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+    setOpacity(0.6);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    setOpacity(0);
+  };
+
+  const handleMouseEnter = () => {
+    setOpacity(0.6);
+  };
+
+  const handleMouseLeave = () => {
+    setOpacity(0);
+  };
+
+  return (
+    <div
+      ref={divRef}
+      onMouseMove={handleMouseMove}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className={`relative rounded-3xl border border-neutral-800 bg-neutral-900 overflow-hidden p-8 ${className}`}
+    >
+      {mode === 'single' && (
+        <div
+          className="pointer-events-none absolute inset-0 opacity-0 transition-opacity duration-500 ease-in-out"
+          style={{
+            opacity,
+            background: `radial-gradient(circle at ${position.x}px ${position.y}px, ${spotlightColor}, transparent 80%)`,
+          }}
+        />
+      )}
+      {mode === 'multicolor' && (
+        <div
+          className="pointer-events-none absolute opacity-0 transition-opacity duration-500 ease-in-out"
+          style={{
+            opacity,
+            left: position.x,
+            top: position.y,
+            width: 420,
+            height: 420,
+            transform: 'translate(-50%, -50%)',
+            borderRadius: '9999px',
+            background: 'conic-gradient(from 0deg at 50% 50%, #4285F4, #EA4335, #FBBC05, #34A853, #4285F4)',
+            filter: 'blur(20px)'
+          }}
+        />
+      )}
+      {children}
+    </div>
+  );
+};
+
+export default SpotlightCard;
